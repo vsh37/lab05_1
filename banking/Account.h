17359e75f1550d5712d86048eb5cdb08c9bafc85
vsh37 <alisa_rudovskaya@mail.ru>
2021-06-29 00:20:37 +0300

    commit 1

---
+++ banking/Account.h
@@ -0,0 +1,26 @@
+#pragma once
+class Account {
+ public:
+  Account(int id, int balance);
+  virtual ~Account();
+
+  // Virtual to test.
+  virtual int GetBalance() const;
+
+  // Virtual to test.
+  virtual void ChangeBalance(int diff);
+
+  // Virtual to test.
+  virtual void Lock();
+
+  // Virtual to test.
+  virtual void Unlock();
+  int id() const { return id_; }
+ 
+// bool operator==(const Account)const{return true;}
+
+ private:
+  int id_;
+  int balance_;
+  bool is_locked_;
+};
